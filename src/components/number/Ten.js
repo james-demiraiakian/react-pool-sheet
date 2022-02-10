9347676db9dff20f4b7436d2266fe21e0c2d262d
James Demiraiakian <james.armen@gmail.com>
2022-02-09 19:15:26 -0800

    finished adding in the bout pairings and order components. These were a pain to do manually, however, due to the nature of bouts and how they were set up, I couldn't see a way to programatically generate these. I'll refactor if I ever come across some way to do that

---
+++ src/components/number/Ten.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import { useList } from '../../context/PartListContext';
+
+export default function Ten() {
+  const { list } = useList();
+  return (
+    <div>
+      <div>
+        {list[0].name} vs {list[3].name}
+      </div>
+      <div>
+        {list[5].name} vs {list[8].name}
+      </div>
+      <div>
+        {list[1].name} vs {list[4].name}
+      </div>
+      <div>
+        {list[6].name} vs {list[9].name}
+      </div>
+      <div>
+        {list[2].name} vs {list[0].name}
+      </div>
+      <div>
+        {list[7].name} vs {list[5].name}
+      </div>
+      <div>
+        {list[3].name} vs {list[4].name}
+      </div>
+      <div>
+        {list[8].name} vs {list[9].name}
+      </div>
+      <div>
+        {list[1].name} vs {list[2].name}
+      </div>
+      <div>
+        {list[6].name} vs {list[7].name}
+      </div>
+      <div>
+        {list[4].name} vs {list[0].name}
+      </div>
+      <div>
+        {list[9].name} vs {list[5].name}
+      </div>
+      <div>
+        {list[3].name} vs {list[1].name}
+      </div>
+      <div>
+        {list[8].name} vs {list[6].name}
+      </div>
+      <div>
+        {list[4].name} vs {list[2].name}
+      </div>
+      <div>
+        {list[9].name} vs {list[7].name}
+      </div>
+      <div>
+        {list[0].name} vs {list[1].name}
+      </div>
+      <div>
+        {list[5].name} vs {list[6].name}
+      </div>
+      <div>
+        {list[2].name} vs {list[3].name}
+      </div>
+      <div>
+        {list[7].name} vs {list[8].name}
+      </div>
+      <div>
+        {list[4].name} vs {list[9].name}
+      </div>
+      <div>
+        {list[0].name} vs {list[5].name}
+      </div>
+      <div>
+        {list[1].name} vs {list[6].name}
+      </div>
+      <div>
+        {list[2].name} vs {list[7].name}
+      </div>
+      <div>
+        {list[3].name} vs {list[8].name}
+      </div>
+      <div>
+        {list[5].name} vs {list[4].name}
+      </div>
+      <div>
+        {list[9].name} vs {list[1].name}
+      </div>
+      <div>
+        {list[7].name} vs {list[0].name}
+      </div>
+      <div>
+        {list[6].name} vs {list[3].name}
+      </div>
+      <div>
+        {list[8].name} vs {list[2].name}
+      </div>
+      <div>
+        {list[1].name} vs {list[5].name}
+      </div>
+      <div>
+        {list[4].name} vs {list[7].name}
+      </div>
+      <div>
+        {list[3].name} vs {list[9].name}
+      </div>
+      <div>
+        {list[0].name} vs {list[8].name}
+      </div>
+      <div>
+        {list[2].name} vs {list[6].name}
+      </div>
+      <div>
+        {list[7].name} vs {list[1].name}
+      </div>
+      <div>
+        {list[5].name} vs {list[3].name}
+      </div>
+      <div>
+        {list[8].name} vs {list[4].name}
+      </div>
+      <div>
+        {list[9].name} vs {list[2].name}
+      </div>
+      <div>
+        {list[6].name} vs {list[0].name}
+      </div>
+      <div>
+        {list[3].name} vs {list[7].name}
+      </div>
+      <div>
+        {list[1].name} vs {list[8].name}
+      </div>
+      <div>
+        {list[2].name} vs {list[5].name}
+      </div>
+      <div>
+        {list[4].name} vs {list[6].name}
+      </div>
+      <div>
+        {list[0].name} vs {list[9].name}
+      </div>
+    </div>
+  );
+}
