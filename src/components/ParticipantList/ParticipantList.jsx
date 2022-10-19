9230c592002a5b90c1b80ba73681833ba5ddb089
James Demiraiakian <james.armen@gmail.com>
2022-10-19 16:05:59 -0700

    some housekeeping. Moved Match.jsx and ParticipantList.jsx into filders to impove navigation of app. Updated imports

--- src/components/ParticipantList/ParticipantList.jsx
+++ src/components/ParticipantList/ParticipantList.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { usePart } from '../context/PartContext';
-import { useList } from '../context/PartListContext';
+import { usePart } from '../../context/PartContext';
+import { useList } from '../../context/PartListContext';
 import { v4 as uuid } from 'uuid';
-import Match from './Match';
+import Match from '../Match/Match';
 import './ParticipantList.css';
 
 export default function ParticipantList() {
@@ -20,7 +20,7 @@ export default function ParticipantList() {
         name: partName,
         victories: 0,
         touchesScored: 0,
-        touchesRecieved: 0,
+        touchesReceived: 0,
         indicator: 0,
         placement: 0,
         id: uuid(),
@@ -59,10 +59,8 @@ export default function ParticipantList() {
         <>
           <h1>Pool Sheet</h1>
           <p>
-            Pool sheet calculator for four to 12 participants. No more than 12 can be added due to
-            the USFA not providing a bout order sheet for larger pools. If more than 12 participants
-            are added, you will have to refresh the page. I plan to add a participant removal
-            feature.
+            Pool sheet calculator for four to twelve participants. No more than 12 can be added due
+            to the USFA not providing a bout order sheet for larger pools.
           </p>
           <form onSubmit={handleAdd}>
             <div>
