ae860f420d1e89840665b660e2793b0f7bfd3650
James Demiraiakian <james.armen@gmail.com>
2023-04-05 13:20:27 -0700

    pushing new branch dev

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './Home.css';
 
 export default function Home() {
-  return <div className="home">Home</div>;
+  return <div className="home">Home (change for dev branch push)</div>;
 }
