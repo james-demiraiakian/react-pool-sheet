1bfb187c38d270706a353c9ac21218d4806bda5b
James Demiraiakian <james.armen@gmail.com>
2023-04-05 15:34:11 -0700

    added on click expansion of historical tournament data

--- src/components/Tournament/List/ListTournament.jsx
+++ src/components/Tournament/List/ListTournament.jsx
@@ -3,6 +3,7 @@ import { getAllTournaments } from '../../../services/tournaments';
 
 export default function ListTournament() {
   const [tournamentList, setTournamentList] = useState([]);
+  const [selected, setSelected] = useState(false);
 
   useEffect(() => {
     const fetch = async () => {
@@ -11,11 +12,22 @@ export default function ListTournament() {
     };
     fetch();
   }, []);
+
+  const handleClick = () => {
+    setSelected(!selected);
+  };
+
   return (
     <div>
       <p>Historical Tournament List</p>
       {tournamentList.map((tournament, i) => (
-        <div key={i}>Tournament {tournament.date}</div>
+        <div key={i}>
+          <div onClick={handleClick}>
+            {/*break lines 25-29 and handleClick function out into own component. Will allow for clicking on individual tournaments to display data independently. Also fetch fencer and bout data to display on the tourny data page*/}
+            Tournament {tournament.date}
+            {selected === true && <div>{tournament.fencerArr}</div>}
+          </div>
+        </div>
       ))}
     </div>
   );
